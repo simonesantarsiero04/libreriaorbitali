adc0d25d8fd78b53544ef425ecca817a9d0e25b8
simonesantarsiero04 <75539291+simonesantarsiero04@users.noreply.github.com>
2023-01-09 18:45:20 +0100

    Update ibridazione_sp.js

--- ibridazione_sp.js
+++ ibridazione_sp.js
@@ -40,9 +40,9 @@ function s_p_alone (scene) {
 
     let material = new BABYLON.StandardMaterial("-mat", scene);
     material.diffuseColor.set(0.9,0.1,0.1);
-    let material2 = new BABYLON.StandardMaterial("-mat", scene);
-    material2.diffuseColor.set(0.9,0.9,0.1);
-    material2.specularColor.set(0.2,0.2,0.2);
+    let material1 = new BABYLON.StandardMaterial("-mat", scene);
+    material1.diffuseColor.set(0.9,0.9,0.1);
+    material1.specularColor.set(0.2,0.2,0.2);
 
     let sphere = MYLIB.Sphere(2,3,position,0,0,0,0,1,1,1,material);
     let drop1 = MYLIB.createDrop(0.6,0.6,0.4,-3,position,0,Math.PI,0,0,1.2,1.2,1.2,material1);
